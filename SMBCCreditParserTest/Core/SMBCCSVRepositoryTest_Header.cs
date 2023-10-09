569aeb857718be4271fb02dec50eacde62d0646a
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2023-10-09 12:27:58 +0900

    データ構造変更テストケース追加

---
+++ SMBCCreditParserTest/Core/SMBCCSVRepositoryTest_Header.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using SMBCCreditParser.Core;
+
+namespace SMBCCreditParserTest.Core
+{
+
+    /// <summary>
+    /// CSVデータのヘッダー値を個別にテスト
+    /// </summary>
+    [TestClass]
+    public class SMBCCSVRepositoryTest_Header
+    {
+        private SMBCCSVRepository _repo = new SMBCCSVRepository();
+
+        private const string propTestContent = """
+			やね　いも　様,4980-03**-****-****,三井住友カードデビュープラスＶＩＳＡ
+			2023/08/08,OPENAI (OPENAI.COM ),5540,１,１,554,3.80　USD　145.980　08 08
+			""";
+       
+        public SMBCCSVRepositoryTest_Header()
+        {
+        }
+
+        [TestMethod]
+        public void 正常データ_プロパティ確認_名前()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent);
+
+            // Assert
+            Assert.AreEqual("やね　いも　様", actual.Items[0].Header.PaymentUserName);
+        }
+
+        [TestMethod]
+        public void 正常データ_プロパティ確認_支払い手段の番号()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent);
+
+            // Assert
+            Assert.AreEqual("4980-03**-****-****", actual.Items[0].Header.PaymentMethodIdentifier);
+        }
+
+        [TestMethod]
+        public void 正常データ_プロパティ確認_支払い手段の名前()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent);
+
+            // Assert
+            Assert.AreEqual("三井住友カードデビュープラスＶＩＳＡ", actual.Items[0].Header.PaymentMethodName);
+        }
+
+
+
+    }
+
+}
---
+++ SMBCCreditParserTest/Core/SMBCCSVRepositoryTest_HeaderAndDetails.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using SMBCCreditParser.Core;
+
+namespace SMBCCreditParserTest.Core
+{
+
+    /// <summary>
+    /// CSVデータのヘッダー・詳細の読み込みをテスト
+    /// </summary>
+    [TestClass]
+    public class SMBCCSVRepositoryTest_HeaderAndDetails
+    {
+        private SMBCCSVRepository _repo = new SMBCCSVRepository();
+
+        private const string propTestContent_1_PaymentMethod = """
+            やね　いも　様,4980-03**-****-****,三井住友カードデビュープラスＶＩＳＡ
+            2023/08/08,OPENAI (OPENAI.COM ),5540,１,１,554,3.80　USD　145.980　08 08
+            2023/07/03,ＰａｙＰａｌ決済,2280,１,１,2280,ＰＡＹＰＡＬ　＊ＧＯＯＧＬＥ　ＹＯＵＴＵ
+            2023/07/23,ＡＰＰＬＥ　ＣＯＭ　ＢＩＬＬ,1980,１,１,1980,
+            """;
+
+        private const string propTestContent_2_PaymentMethod = """
+            やね　いも　様,4980-03**-****-****,三井住友カードデビュープラスＶＩＳＡ
+            2023/08/08,OPENAI (OPENAI.COM ),5540,１,１,554,3.80　USD　145.980　08 08
+            2023/07/03,ＰａｙＰａｌ決済,2280,１,１,2280,ＰＡＹＰＡＬ　＊ＧＯＯＧＬＥ　ＹＯＵＴＵ
+            2023/07/23,ＡＰＰＬＥ　ＣＯＭ　ＢＩＬＬ,1980,１,１,1980,
+            やね　いも　様,6900-11**-****-****,ＡｐｐｌｅＰａｙ／ｉＤ
+            2023/07/01,ローソン／ｉＤ,214,１,１,214,ﾛ-ｿﾝ ｱﾘﾉﾁﾖｳﾆﾛｳ/ID
+            2023/07/30,ユニクロ／ｉＤ,1990,１,１,1990,
+            """;
+
+        public SMBCCSVRepositoryTest_HeaderAndDetails()
+        {
+        }
+
+        [TestMethod]
+        public void ヘッダー_詳細の読み込み_1_全体件数()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent_1_PaymentMethod);
+
+            // Assert
+            Assert.AreEqual(1, actual.Items.Count);
+        }
+
+        [TestMethod]
+        public void ヘッダー_詳細の読み込み_1_詳細件数()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent_1_PaymentMethod);
+
+            // Assert
+            Assert.AreEqual(3, actual.Items[0].Details.Count);
+        }
+
+        [TestMethod]
+        public void ヘッダー_詳細の読み込み_2_全体件数()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent_2_PaymentMethod);
+
+            // Assert
+            Assert.AreEqual(2, actual.Items.Count);
+        }
+
+        [TestMethod]
+        public void ヘッダー_詳細の読み込み_2_詳細件数()
+        {
+            // Act
+            var actual = _repo.GetSMBCCSV(propTestContent_2_PaymentMethod);
+
+            // Assert
+            Assert.AreEqual(2, actual.Items[0].Details.Count);
+        }
+
+    }
+
+}
