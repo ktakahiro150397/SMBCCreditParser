183c53b638814de97b31632e486b20cf48c12c50
ktakahiro150397 <k_takahiro_150397@yahoo.co.jp>
2023-10-09 12:12:02 +0900

    NLog出力設定追加

--- SMBCCreditParser/Program.cs
+++ SMBCCreditParser/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace SMBCCreditParser
+﻿using NLog;
+
+namespace SMBCCreditParser
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Logger _logger = LogManager.GetCurrentClassLogger();
+
+            _logger.Trace("Trace ログです。");
+            _logger.Debug("Debug ログです。");
+            _logger.Info("Info ログです。");
+            _logger.Warn("Warn ログです。");
+            _logger.Error("Error ログです。");
+            _logger.Fatal("Fatal ログです。");
         }
     }
 }
